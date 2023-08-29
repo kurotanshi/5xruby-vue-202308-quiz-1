63108479807fa890f366095d9bc42f5eecb94ecf
HotDogCat <g8xk7su3@gmail.com>
2023-08-29 19:42:42 +0800

    完成(2/3)

--- ubike/src/App.vue
+++ ubike/src/App.vue
@@ -2,8 +2,8 @@
 import { ref, computed, watch } from 'vue';
 
 // 修改這份 YouBike 即時資訊表，並加上
-// 1. 站點名稱搜尋
-// 2. 目前可用車輛 / 總停車格 的排序功能
+// 1. 站點名稱搜尋 V
+// 2. 目前可用車輛 / 總停車格 的排序功能 V
 // 3. 加入分頁功能，每頁 20 筆資料
 
 // 欄位說明:
@@ -14,24 +14,61 @@ import { ref, computed, watch } from 'vue';
 // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
 
 const uBikeStops = ref([]);
-
+const inputName = ref('');
+const sbiAsc = ref(false);
+const totAsc = ref(false);
 fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
   .then(res => res.text())
   .then(data => {
     uBikeStops.value = JSON.parse(data);
   });
 
+  // 搜尋排序
+const filterStops = computed({
+  get: () => inputName.value ? uBikeStops.value.filter(item => item.sna.match(inputName.value)) : uBikeStops.value,
+  set: (val) => {
+    if(val.choose == 'sbi'){
+      if (val.sort) {
+        uBikeStops.value.sort( (x,y) => x.sbi > y.sbi ? 1: -1 )
+      }else{
+        uBikeStops.value.sort( (x,y) => x.sbi < y.sbi ? 1: -1 )
+      }
+    }
+    if(val.choose == 'tot'){
+      if(val.sort) {
+        uBikeStops.value.sort( (x,y) => x.tot > y.tot ? 1: -1 )
+      }else{
+        uBikeStops.value.sort( (x,y) => x.tot < y.tot ? 1: -1 )
+      }
+    }
+  }
+})
+
+// click 觸發排序 
+const sbiSort = () => {
+  sbiAsc.value = !sbiAsc.value;
+  filterStops.value = { sort:sbiAsc.value, choose:'sbi' }
+  
+}
+const totSort = () => {
+  totAsc.value = !totAsc.value;
+  filterStops.value = { sort:totAsc.value, choose:'tot' }
+}
+
+
 const timeFormat = (val) => {
   // 時間格式
   const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
   return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
 };
+
+
 </script>
 
 <template>
 <div class="app">
   <p>
-    站點名稱搜尋: <input type="text">
+    站點名稱搜尋: <input type="text" v-model="inputName">
   </p>
 
   <table class="table table-striped">
@@ -41,18 +78,16 @@ const timeFormat = (val) => {
         <th>場站名稱</th>
         <th>場站區域</th>
         <th>目前可用車輛
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          <a href=""  @click.prevent="sbiSort"><i class="fa fa-sort-asc" aria-hidden="true"></i><i class="fa fa-sort-desc" aria-hidden="true"></i></a>
         </th>
         <th>總停車格
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          <a href=""  @click.prevent="totSort"><i class="fa fa-sort-asc" aria-hidden="true"></i><i class="fa fa-sort-desc" aria-hidden="true"></i></a>
         </th>
         <th>資料更新時間</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="s in uBikeStops" :key="s.sno">
+      <tr v-for="s in filterStops" :key="s.sno">
         <td>{{ s.sno }}</td>
         <td>{{ s.sna }}</td>
         <td>{{ s.sarea }}</td>
