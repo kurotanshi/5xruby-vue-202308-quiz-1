826adb695a2907163ca6f08d3766a703dfd70bfa
Midomommy <tinasue165@gmail.com>
2023-08-31 00:25:17 +0800

    final code

--- make-interactive-ratings/src/App.vue
+++ make-interactive-ratings/src/App.vue
@@ -4,128 +4,80 @@
 1. 將 movies 的內容用 v-for 改寫成動態渲染
 2. 完成電影星星評分的事件綁定，同樣用 v-for 進行改寫
 */
-import { ref } from "vue";
+import { ref, computed, watch } from "vue";
 import { StarIcon } from "@heroicons/vue/24/solid";
 import { items } from "./movies.json";
 
 const movies = ref(items);
+
+const ratingText = (value) => {
+  return `Rating: (${value}/5)`
+}
+
+const changeRatingStar = (color, num, mid) => {
+  const movie = movies.value.find(movie => movie.id === mid);
+
+  if (color === 'y') {
+    movie.rating = num;
+  } else if (color === 'g') {
+    movie.rating += num;
+  }
+
+}
+
+const movieRating = computed(() =>
+  movies.value.reduce((acc, item) => {
+    acc[item.id] = {
+      yellowStar: item.rating,
+      grayStar: 5 - item.rating
+    }
+    return acc;
+  }, {})
+);
+
 </script>
 
 <template>
-<div class="app">
-  <div class="movie-list">
-    <div class="movie-item">
-      <div class="movie-item-image-wrapper">
-        <img src="https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UY1982_.jpg" class="movie-item-image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">The Godfather</h3>
-          <div class="movie-item-genres-wrapper">
-            <span class="movie-item-genre-tag">Crime</span>
-            <span class="movie-item-genre-tag">Drama</span>
+  <div class="app">
+    <div class="movie-list">
+      <div class="movie-item" v-for="m in movies" :key="m.id">
+        <div class="movie-item-image-wrapper">
+          <img :src="m.image" class="movie-item-image" alt="">
+        </div>
+        <div class="movie-item-content-wrapper">
+          <div class="movie-item-title-wrapper">
+            <h3 class="movie-item-title">{{ m.name }}</h3>
+            <div class="movie-item-genres-wrapper">
+              <span class="movie-item-genre-tag" v-for=" g in m.genres">{{ g }}</span>
+            </div>
           </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: (4/5) </span>
-          
-          <div class="movie-item-star-icon-wrapper">
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
+          <div class="movie-item-description-wrapper">
+            <p class="movie-item-description">{{ m.description }}</p>
           </div>
-        </div>
-      </div>
-    </div>
-    <div class="movie-item">
-      <div class="movie-item-image-wrapper">
-        <img src="https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1200_.jpg" class="movie-item-image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">The Shawshank Redemption</h3>
-          <div class="movie-item-genres-wrapper">
-            <span class="movie-item-genre-tag">Drama</span>
-          </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: (4/5) </span>
-          <div class="movie-item-star-icon-wrapper">
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="movie-item">
-      <div class="movie-item-image-wrapper">
-        <img src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UY2048_.jpg" class="movie-item-image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">The Dark Knight</h3>
-          <div class="movie-item-genres-wrapper">
-            <span class="movie-item-genre-tag">Action</span>
-            <span class="movie-item-genre-tag">Crime</span>
-            <span class="movie-item-genre-tag">Drama</span>
-          </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: (3/5) </span>
-          <div class="movie-item-star-icon-wrapper">
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
+          <div class="movie-item-rating-wrapper" v-if="movieRating[m.id]">
+            <span class="movie-item-rating-text" v-text="ratingText(movieRating[m.id].yellowStar)"></span>
+            <div class="movie-item-star-icon-wrapper">
+              <template v-for="(starCount, color) in movieRating[m.id]">
+                <button v-for="(count, idx) in starCount" :key="`${m.id}_${color}_${idx}`" :class="{
+                  'text-yellow-500': color === 'yellowStar',
+                  'text-gray-500': color === 'grayStar',
+                }" @click="changeRatingStar(color.charAt(0), idx + 1, m.id)">
+                  <StarIcon class="movie-item-star-icon" />
+                </button>
+              </template>
+              <!-- <button class="text-yellow-500 movie-item-star-icon-button"
+                v-for=" (y, idx) in movieRating[m.id].yellowStar"
+                @click="changeRatingStar('y', idx + 1, m.id)">
+                <StarIcon class="movie-item-star-icon" />
+              </button>
+              <button class="text-gray-500 movie-item-star-icon-button" v-for=" (g, idx) in movieRating[m.id].grayStar"
+                @click="changeRatingStar('g', idx + 1, m.id)">
+                <StarIcon class="movie-item-star-icon" />
+              </button> -->
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
--- ubike/src/App.vue
+++ ubike/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch, onMounted } from 'vue';
 
 // 修改這份 YouBike 即時資訊表，並加上
 // 1. 站點名稱搜尋
@@ -14,11 +14,28 @@ import { ref, computed, watch } from 'vue';
 // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
 
 const uBikeStops = ref([]);
+const uBikeStopsFilter = ref([]);
+// const uBikeStopsFilterPaginated = ref([]);
+
+const searchTxt = ref("");
+const isSbiAsc = ref(false);
+const isTotAsc = ref(false);
+
+const perPageNum = ref(20);
+const activePage = ref(0);
+const activePageGroup = ref(0);
+
+const isPrevBtn = ref(false);
+const isNextBtn = ref(true);
+
+// const paginatedArray = ref(0)
 
 fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
   .then(res => res.text())
   .then(data => {
-    uBikeStops.value = JSON.parse(data);
+    const parsedData = JSON.parse(data);
+    uBikeStops.value = parsedData;
+    uBikeStopsFilter.value = parsedData;
   });
 
 const timeFormat = (val) => {
@@ -26,47 +43,202 @@ const timeFormat = (val) => {
   const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
   return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
 };
+
+//處理搜尋
+const stopsSearch = () => {
+  if (searchTxt.value)
+    uBikeStopsFilter.value = uBikeStops.value.filter(stop => stop.sna.includes(searchTxt.value));
+  else
+    uBikeStopsFilter.value = uBikeStops.value;
+  activePage.value = 0;
+  activePageGroup.value = 0;
+}
+
+watch(uBikeStopsFilter, () => {
+  if (uBikeStopsFilter.value.length <= perPageNum.value * 10) {
+    isPrevBtn.value = false;
+    isNextBtn.value = false;
+  } else {
+    isPrevBtn.value = false;
+    isNextBtn.value = true;
+  }
+})
+
+//處理排序
+const toggleAsc = (val) => {
+  if (val === 'sbi') {
+    isSbiAsc.value = !isSbiAsc.value;
+  } else if (val === 'tot') {
+    isTotAsc.value = !isTotAsc.value;
+  }
+}
+
+watch(isSbiAsc, (newSbiAsc) => {
+  sortUbikeStops(newSbiAsc, 'sbi');
+})
+
+watch(isTotAsc, (newTotAsc) => {
+  sortUbikeStops(newTotAsc, 'tot');
+})
+
+function sortUbikeStops(ase, property) {
+  const order = ase ? 1 : -1;
+  uBikeStopsFilter.value.sort((a, b) => (a[property] - b[property]) * order)
+
+  activePage.value = 0;
+  activePageGroup.value = 0;
+}
+
+//處理分頁功能
+//1. 先把資料分組
+//2. 把分好組的資料拿來作分頁
+const uBikeStopsFilterPaginated = computed(() => {
+  return paginatedUbikeStops(uBikeStopsFilter.value, perPageNum.value);
+})
+
+function paginatedUbikeStops(data, itemPerpage) {
+  const paginatedData = [];
+
+  for (let i = 0; i < data.length; i += itemPerpage) {
+    paginatedData.push(data.slice(i, i + itemPerpage));
+  }
+  return paginatedData;
+}
+
+const showPaginatedStops = computed(() => {
+  return uBikeStopsFilterPaginated.value[activePage.value];
+})
+
+const paginatedArray = computed(() => {
+  const paginationTotalNum = uBikeStopsFilterPaginated.value.length;
+  const tempArr = [];
+
+  for (let i = 0; i < paginationTotalNum; i++) {
+    tempArr.push(i);
+  }
+
+  const pageArrayObj = [];
+
+  for (let i = 0; i < tempArr.length; i += 10) {
+    pageArrayObj.push(tempArr.slice(i, i + 10))
+  }
+  return pageArrayObj;
+})
+
+const paginationShowGroup = computed(() => {
+  return paginatedArray.value[activePageGroup.value]
+})
+
+function showPage(idx) {
+  activePage.value = idx;
+}
+
+function changePageGroup(navigation) {
+  const lastIndex = paginatedArray.value.length - 1;
+  const maxPage = uBikeStopsFilterPaginated.value.length - 1;
+
+  if (navigation === 'prev') {
+    activePageGroup.value = Math.max(activePageGroup.value - 1, 0);
+    activePage.value = Math.max(activePage.value - 10, 0);
+  } else if (navigation === 'next') {
+    if (activePageGroup.value < lastIndex) {
+      activePage.value = Math.min(activePage.value + 10, maxPage);
+      activePageGroup.value += 1;
+    } else {
+      activePage.value = paginatedArray.value[lastIndex][0];
+    }
+  }
+  
+  isPrevBtn.value = activePageGroup.value > 0;
+  isNextBtn.value = activePageGroup.value < lastIndex;
+}
+
+
+// function changePageGroup(navigation) {
+//   if (navigation === 'prev') {
+//     activePageGroup.value ? activePageGroup.value -= 1 : activePageGroup.value = 0;
+//     activePage.value -= 10;
+//   } else if (navigation === 'next') {
+//     if (activePageGroup.value < paginatedArray.value.length - 1) {
+//       activePage.value = (activePage.value + 10 < uBikeStopsFilterPaginated.value.length) ? activePage.value + 10 : uBikeStopsFilterPaginated.value.length - 1;
+//       activePageGroup.value += 1;
+//     } else if (activePageGroup.value === paginatedArray.value.length - 1) {
+//       activePage.value = paginatedArray.value[paginatedArray.value.length][0];
+//       activePageGroup.value = paginatedArray.value.length - 1;
+//     }
+//   }
+//   isPrevBtn.value = activePageGroup.value > 0;
+//   isNextBtn.value = activePageGroup.value < paginatedArray.value.length - 1;
+
+// }
+
 </script>
 
 <template>
-<div class="app">
-  <p>
-    站點名稱搜尋: <input type="text">
-  </p>
+  <div class="app">
+    <p>
+      站點名稱搜尋: <input type="text" v-model="searchTxt" @keyup.enter="stopsSearch()" placeholder="輸入完後請按下Enter鍵">
+    </p>
+    <p>資料總筆數{{ uBikeStopsFilter.length }}</p>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>場站名稱</th>
+          <th>場站區域</th>
+          <th class="sort-style" @click="toggleAsc('sbi')">
+            目前可用車輛
+            <i :class="[{ 'fa-sort-asc': isSbiAsc }, 'fa']" aria-hidden="true"></i>
+            <i :class="[{ 'fa-sort-desc': !isSbiAsc }, 'fa']" aria-hidden="true"></i>
+          </th>
+          <th class="sort-style" @click="toggleAsc('tot')">總停車格
+            <i :class="[{ 'fa-sort-asc': isTotAsc }, 'fa']" aria-hidden="true"></i>
+            <i :class="[{ 'fa-sort-desc': !isTotAsc }, 'fa']" aria-hidden="true"></i>
+          </th>
+          <th>資料更新時間</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="s in showPaginatedStops" :key="s.sno">
+          <td>{{ s.sno }}</td>
+          <td>{{ s.sna }}</td>
+          <td>{{ s.sarea }}</td>
+          <td>{{ s.sbi }}</td>
+          <td>{{ s.tot }}</td>
+          <td>{{ timeFormat(s.mday) }}</td>
+        </tr>
+      </tbody>
+    </table>
 
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>場站名稱</th>
-        <th>場站區域</th>
-        <th>目前可用車輛
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
-        </th>
-        <th>總停車格
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
-        </th>
-        <th>資料更新時間</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="s in uBikeStops" :key="s.sno">
-        <td>{{ s.sno }}</td>
-        <td>{{ s.sna }}</td>
-        <td>{{ s.sarea }}</td>
-        <td>{{ s.sbi }}</td>
-        <td>{{ s.tot }}</td>
-        <td>{{ timeFormat(s.mday) }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <div v-if="uBikeStopsFilter.length <= 0" class="no-data">
+      <p>sorry,there's no data.</p>
+    </div>
+    <nav v-if="uBikeStopsFilter.length > 0" class="page-navigation" aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li :class="[{ 'disabled': !isPrevBtn }, 'page-item']"><a class="page-link" href=""
+            @click.prevent="changePageGroup('prev')">Previous 10 Pages</a></li>
+        <template v-for="idx in paginationShowGroup ">
+          <li :class="[{ 'active': idx === activePage }, 'page-item']"><a class="page-link" href=""
+              @click.prevent="showPage(idx)">{{ idx + 1 }}</a></li>
+        </template>
+        <li :class="[{ 'disabled': !isNextBtn }, 'page-item']"><a class="page-link" href=""
+            @click.prevent="changePageGroup('next')">Next 10 Pages</a></li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
 .app {
   padding: 1rem;
+}
+
+th.sort-style:hover {
+  cursor: pointer;
+}
+
+.no-data {
+  text-align: center;
+  color: gray;
 }
 </style>
