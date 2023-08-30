2621443e10be51ec0291f9c7082e3a250394b57b
jcyenyen <amanda7776@gmail.com>
2023-08-30 23:34:49 +0800

    完成排序功能 分頁功能

--- ubike/src/App.vue
+++ ubike/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, watch} from 'vue';
+import { ref, computed, watch,} from 'vue'
 
 // 修改這份 YouBike 即時資訊表，並加上
 // 1. 站點名稱搜尋
@@ -13,80 +13,144 @@ import { ref, computed, watch} from 'vue';
 // lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
 // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
 
-const uBikeStops = ref([]);
+const uBikeStops = ref([])
 
-// const uBikeData = ref([])
-// const snaSearch = ref('')
-
-fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
-  .then(res => res.text())
-  .then(data => {
-    uBikeStops.value = JSON.parse(data);
+fetch(
+  'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+)
+  .then((res) => res.text())
+  .then((data) => {
+    uBikeStops.value = JSON.parse(data)
     uBikeData.value = JSON.parse(data)
-  });
-  
+    // 非同步 頁籤先在這處理
+    pages.value = Math.ceil(uBikeData.value.length / 20)-50
+    console.log(pages.value)
+    uBikeStops.value.length = 20
+  })
+
 const timeFormat = (val) => {
   // 時間格式
-  const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
-  return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
-};
+  const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/
+  return val.replace(pattern, '$1/$2/$3 $4:$5:$6')
+}
 
 // 搜尋站名
-
-const uBikeData = ref([])
+const uBikeData = ref( )
 const snaSearch = ref('')
 
-watch(snaSearch,(newSna)=>{
-  const searchStops = uBikeData.value.filter((v)=>v.sna.indexOf(newSna) !== -1)
+watch(snaSearch, (newSna) => {
+  const searchStops = uBikeData.value.filter(
+    (v) => v.sna.indexOf(newSna) !== -1
+  )
   uBikeStops.value = searchStops
+  console.log(uBikeStops.value)
 })
 
-// const newUBikeStops = computed(()=>{return uBikeStops.value.filter((v)=>{
-//   return v.sna.indexOf(snaSearch.value) !== -1
-// })})
+//排序
 
+const sort = (sortWay, sortFor) => {
+  if (sortWay === 'asc') {
+    uBikeStops.value.sort((a, b) => {
+      return a[sortFor] - b[sortFor]
+    })
+  } else {
+    uBikeStops.value.sort((a, b) => {
+      return b[sortFor] - a[sortFor]
+    })
+  }
+}
+
+// 頁籤
+  const pages = ref()
+  const pageFor = (page) =>{
+    uBikeStops.value = uBikeData.value.slice((page-1)*20,((page-1)*20)+19) 
+  }
+  const pageMove = (way) =>{
+    if (pages.value <= 0)return
+    way === 'left' ?pages.value = pages.value -10 :pages.value= pages.value + 10
+  }
 
 </script>
 
 <template>
-<div class="app">
-  <p>
-    站點名稱搜尋: <input type="text" v-model="snaSearch">
-  </p>
+  <div class="app">
+    <p>站點名稱搜尋: <input type="text" v-model="snaSearch" /></p>
 
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>場站名稱</th>
-        <th>場站區域</th>
-        <th>目前可用車輛
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
-        </th>
-        <th>總停車格
-          <i class="fa fa-sort-asc" aria-hidden="true"></i>
-          <i class="fa fa-sort-desc" aria-hidden="true"></i>
-        </th>
-        <th>資料更新時間</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="s in uBikeStops" :key="s.sno">
-        <td>{{ s.sno }}</td>
-        <td>{{ s.sna }}</td>
-        <td>{{ s.sarea }}</td>
-        <td>{{ s.sbi }}</td>
-        <td>{{ s.tot }}</td>
-        <td>{{ timeFormat(s.mday) }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>場站名稱</th>
+          <th>場站區域</th>
+          <th>
+            目前可用車輛
+            <i
+              class="fa fa-sort-asc"
+              aria-hidden="true"
+              @click="sort('asc', 'sbi')"
+            ></i>
+            <i
+              class="fa fa-sort-desc"
+              aria-hidden="true"
+              @click="sort('desc', 'sbi')"
+            ></i>
+          </th>
+          <th>
+            總停車格
+            <i
+              class="fa fa-sort-asc"
+              aria-hidden="true"
+              @click="sort('asc', 'tot')"
+            ></i>
+            <i
+              class="fa fa-sort-desc"
+              aria-hidden="true"
+              @click="sort('desc', 'tot')"
+            ></i>
+          </th>
+          <th>資料更新時間</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="s in uBikeStops" :key="s.sno">
+          <td>{{ s.sno }}</td>
+          <td>{{ s.sna }}</td>
+          <td>{{ s.sarea }}</td>
+          <td>{{ s.sbi }}</td>
+          <td>{{ s.tot }}</td>
+          <td>{{ timeFormat(s.mday) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <ul>
+    <li class="pager"><a href="#" @click="pageMove('left')"> &lt; </a></li>
+    <li class="pager" v-for="n of pages">
+      <!-- n 從 1 開始計算 -->
+      <a href="#" @click="pageFor(n)">{{ n }}</a>
+    </li>
+    <li class="pager"><a href="#" @click="pageMove('right')" > &gt; </a></li>
+  </ul>
 </template>
 
 <style scoped>
 .app {
   padding: 1rem;
+}
+.pager {
+  float: left;
+  display: block;
+  width: 40px;
+  height: 30px;
+  text-align: center;
+  line-height: 20px;
+  margin-right: 5px;
+  padding: 5px;
+  border: 1px solid #aaa;
+  margin-bottom: 30px;
+}
+
+.pager a {
+  text-decoration: none;
 }
 </style>
